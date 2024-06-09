57f1b5e63c5c04303b5b475632d68e7754ab6a22
Hasan-XS <ha3an.majidii@gmail.com>
2024-06-09 15:53:06 +0330

    Register animes article in admin for show in admin page

--- anime/admin.py
+++ anime/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ArticleAnime
 # Register your models here.
+
+admin.site.register(ArticleAnime)
--- anime/models.py
+++ anime/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class ArticleAnime(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
-    time = models.IntegerChoices()
+    time = models.IntegerField(default=5)
