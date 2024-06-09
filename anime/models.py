dbc94f57bed557cb74ce326f863d210beb97bb3e
Hasan-XS <ha3an.majidii@gmail.com>
2024-06-09 15:54:19 +0330

    fix bug

--- anime/models.py
+++ anime/models.py
@@ -7,3 +7,6 @@ class ArticleAnime(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     time = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.title
